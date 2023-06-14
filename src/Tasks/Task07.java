fb3bcd3a052b976178a2b59acda5e83274adca5e
Ahmet_cakir <126236515+ahmetcakir062023@users.noreply.github.com>
2023-06-14 17:48:40 +0300

    Initial commit

---
+++ src/Tasks/Task07.java
@@ -0,0 +1,9 @@
+package Tasks;
+
+public class Task07 {
+    public static void main(String[] args) {
+        // task-> girilen  5 sayının  en büyüğünü print den code create eediniz
+
+
+    }
+}
---
+++ src/Tasks/Task09.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task09 {
+    public static void main(String[] args) {
+
+
+        // kullanicidan kac sayi toplamak istedigini alin
+        // bu sayilari alin ve bu sayilarin toplamini ekrana yazdirin
+
+
+
+    }
+
+}
